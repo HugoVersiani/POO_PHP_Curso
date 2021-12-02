643dc01a26dc6d9842832e735771f3818ee3368c
Hugo <hugoversiani@live.com>
2021-12-02 07:29:08 -0300

    Novas atualizaçoes nos estudos

---
+++ constructor_destructor.php
@@ -0,0 +1,14 @@
+<?php 
+
+    class Pessoa {
+
+        public $nome = null;
+        function __construct() {
+            echo 'Objeto iniciado';
+        }
+
+    }
+
+    $pessoa1 = new Pessoa();
+
+?>
